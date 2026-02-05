d59d59049d4171b3050acc33bedbac81314227f8
Seongjun <whtjd0709@naver.com>
2026-02-05 17:47:36 +0900

    26년 02월 25일 DB Connect - Statement 사용

---
+++ src/dbutil/DBSelectTest.java
@@ -0,0 +1,56 @@
+package dbutil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBSelectTest {
+    public static void main(String[] args) {
+        Connection comm = null;
+        //1. Connection 연결 객체 생성 (DriverManager.getConection())
+        //2. SQL 작성
+        //3. Connection 객체에서 Statement 객체를 생성.
+        //4. 생성된 Statement 객체를 이용해서 SQL을 실행.(Insert)
+        //5. 이 후 작업이 있으면, 없으면 종료..
+
+        //연결을 위한 정보
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String user = "jdbcuser";
+        String password = "jdbcuser";
+
+        // 1. 객체 생성
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            //2. SQL 작성
+            String sql = "Select *from person";
+
+            //3.Statement 객체 생성, ResultSet 객체 생성 (Query 결과 받을 객체)
+            Statement stmt = conn.createStatement();
+            ResultSet rs = null;
+            //4. SQL을 실행
+            //Select의 실행은 executeQuery(sql)을 사용함
+            //ResultSet 객체로 반환합니다
+            rs = stmt.executeQuery(sql);
+            //5. 결과 확인
+            while (rs.next()){ //rs.next() 반환값은 boolean!!
+                System.out.println("id : " +  rs.getLong("id"));
+                System.out.println("userId : " +  rs.getString("userId"));
+                System.out.println("userPw : " +  rs.getString("userPw"));
+                System.out.println("userEmail : " +  rs.getString("userEmail"));
+                System.out.println("age : " +  rs.getByte("age"));
+                System.out.println("Phone : " +  rs.getString("phone1")
+                            + "-" + rs.getString("phone2"));
+                System.out.println("address1 : " +  rs.getString("address1"));
+                System.out.println("address2 : " +  rs.getString("address2"));
+                System.out.println("regDate : " +  rs.getTimestamp("regDate"));
+                System.out.println("modifyDate : " +  rs.getTimestamp("modifyDate"));
+            // }else{
+                
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/domain/PersonVO.java
@@ -0,0 +1,144 @@
+package domain;
+
+import java.sql.Timestamp;
+
+public class PersonVO {
+
+    // 멤버 변수 선언
+    private long id;
+    private String userId;
+    private String userPw;
+    private String userName;
+    private String userEmail;
+    private String phone1;
+    private String phone2;
+    private byte age;
+    private String address1;
+    private String address2;
+    private Timestamp regDate;
+    private Timestamp modifyDate;
+
+    // 생성자
+    // 기본 생성자
+    public PersonVO() {
+    }
+
+    // 빌드 생성자
+    public PersonVO(String userId, String userPw, String userName, String userEmail, String phone1, String phone2,
+            byte age, String address1, String address2) {
+        this.userId = userId;
+        this.userPw = userPw;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.age = age;
+        this.address1 = address1;
+        this.address2 = address2;
+    }
+
+    // 메서드(Getter, Setter)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public void setUserPw(String userPw) {
+        this.userPw = userPw;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public Timestamp getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Timestamp regDate) {
+        this.regDate = regDate;
+    }
+
+    public Timestamp getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Timestamp modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonVO [id=" + id + ", userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail
+                + ", phone1=" + phone1 + ", phone2=" + phone2 + ", age=" + age + ", address1=" + address1
+                + ", address2=" + address2 + ", regDate=" + regDate + ", modifyDate=" + modifyDate + "]";
+    }
+
+}
